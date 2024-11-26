4e4255ec0d87fbb71aa9765c493a9124367ab38d
Emirlan <nur.emirlan.02@gmail.com>
2024-11-26 15:41:09 +0600

    add dowload functionallity

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,6 @@
 import React from 'react';
 
 const ProductCard = ({ product }) => {
-
-  const handleDownload = () => {
-    const link = document.createElement('a');
-    link.href = '../assets/pdf/dogal.pdf';
-    link.click();
-  };
-
   return (
     <div className="flex justify-between items-start p-4 bg-white shadow-md rounded-lg mb-4 max-[768px]:flex-wrap">
       {/* Левая часть - Изображение и информация о продукте */}
@@ -15,7 +8,7 @@ const ProductCard = ({ product }) => {
         <img src={product.image} alt={product.name} className="w-20 h-20 object-cover rounded-md" />
         <div>
           <h3 className="text-lg font-semibold">{product.name}</h3>
-          <p className="text-sm text-gray-500">{product.description}</p>
+          <p className="text-sm text-gray-500 truncate-text">{product.description}</p>
           <p className="text-xs text-gray-400 mt-1">{product.specs}</p> 
         </div>
       </div>
@@ -24,7 +17,9 @@ const ProductCard = ({ product }) => {
       <div className="flex flex-col items-end space-y-2">
         <p className="text-xl font-bold text-center text-green-600">{product.price} сом</p>
         <div className="flex space-x-2 max-[768px]:flex-wrap">
-          <button onClick={handleDownload} className="px-4 py-1 m-1 bg-orange-400 hover:bg-orange-500 text-white rounded">Подробнее</button>
+          <a href="../../public/assets/pdf/dogal.pdf" download="dogal.pdf">
+          <button className="px-4 py-1 m-1 bg-orange-400 hover:bg-orange-500 text-white rounded">Подробнее</button>
+          </a>
           <button className="px-4 py-1 m-1 bg-green-500 hover:bg-green-600 text-white rounded">Купить</button>
         </div>
       </div>
