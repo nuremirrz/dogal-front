3a77bd8198486285cfcc49121d49f81685d0b332
Emirlan <nur.emirlan.02@gmail.com>
2025-01-10 15:26:30 +0600

    remove img

--- src/components/CustomCarousel.jsx
+++ src/components/CustomCarousel.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Carousel } from 'antd';
-import sliderLogo from '../assets/images/slider.png';
 import sliderLogo2 from '../assets/images/slider2.png'
 import sliderLogo3 from '../assets/images/slider3.png'
 import sliderLogo4 from '../assets/images/slider4.jpg'
@@ -11,13 +10,10 @@ const CustomCarousel = () => {
     <div className="carousel-container">
       <Carousel autoplay autoplaySpeed={5000} draggable>
         <div>
-          <img className="carousel-image" src={sliderLogo} alt="Slider 1" />
+          <img className="carousel-image" src={sliderLogo3} alt="Slider 3" />
         </div>
         <div>
           <img className="carousel-image" src={sliderLogo2} alt="Slider 2" />
-        </div>
-        <div>
-          <img className="carousel-image" src={sliderLogo3} alt="Slider 3" />
         </div>
         <div>
           <img className="carousel-image" src={sliderLogo4} alt="Slider 4" />
