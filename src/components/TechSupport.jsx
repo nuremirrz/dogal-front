7ec2152b915e7dddaefc29d686d52d3ff9b93c6b
Emirlan <nur.emirlan.02@gmail.com>
2025-01-23 15:51:15 +0600

    text correction

--- src/components/TechSupport.jsx
+++ src/components/TechSupport.jsx
@@ -77,7 +77,7 @@ const TechSupport = () => {
         <div className="container tech-sup-container" ref={containerRef}>
             <h2 className="text-3xl text-center m-8 font-semibold max-[480px]:text-2xl max-[480px]:mb-6 relative">
                 <span
-                    className="text-green-50 rounded-xl px-5 py-2 font-custom bg-green-600 max-[480px]:px-4 transform transition-transform duration-500 hover:scale-110"
+                    className="text-green-50 rounded-xl px-5 py-2 font-custom bg-green-600 max-[480px]:px-4 transform transition-transform duration-500 hover:scale-110 max-[480px]:text-xxl"
                     style={{ display: 'inline-block', boxShadow: '0 4px 10px rgba(0,0,0,0.3)', borderRadius: '10px' }}
                 >
                     Сотрудники технической поддержки в {slugPlace}
