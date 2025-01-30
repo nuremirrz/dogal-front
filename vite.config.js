15ad550afdea5ed1ca771067bf459378b4666611
Emirlan <nur.emirlan.02@gmail.com>
2025-01-30 16:01:32 +0600

    add dotenv-rework-vite-config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import dotenv from 'dotenv';
 
-// https://vitejs.dev/config/
+// Загружаем переменные окружения из .env
+dotenv.config();
+
 export default defineConfig({
   plugins: [react()],
   server: {    
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_API_URL || 'http://localhost:5000',
+        target: process.env.VITE_API_URL || 'http://localhost:5000',
         changeOrigin: true,
       },
     },
-    
   },
   assetsInclude: ['**/*.jpg', '**/*.JPG', '**/*.jpeg', '**/*.JPEG'],
 });
