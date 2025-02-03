f231c0e060424bcd24a599d6639cdfce5948ecbb
Emirlan <nur.emirlan.02@gmail.com>
2025-02-03 12:32:05 +0600

    rework vite

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,8 @@ export default defineConfig({
   server: {    
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:5000',
+        // target: process.env.VITE_API_URL || 'http://localhost:5000',
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
       },
     },
